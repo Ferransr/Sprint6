90da29190fecb1b81925c7e0cffa59bfd947618e
Ferransr <65186873+Ferransr@users.noreply.github.com>
2023-07-06 22:35:16 +0200

    github pages changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ferransr.github.io/sprint6",
+  base: "https://ferransr.github.io/Sprint6/"
 })
